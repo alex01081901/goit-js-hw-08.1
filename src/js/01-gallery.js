8635aa5625806401ddaf4ca829beeba0ce910249
alex01081901 <114284497+alex01081901@users.noreply.github.com>
2023-04-01 19:00:46 +0300

    ytghjmtxgyu

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,23 +1,23 @@
-// // Add imports above this line
-// import '../../node_modules/simple-lightbox/dist/simpleLightbox.css';
+// Add imports above this line
+import '../../node_modules/simple-lightbox/dist/simpleLightbox.css';
 
-// import { galleryItems } from './gallery-items';
-// // Change code below this line
-// import SimpleLightbox from 'simple-lightbox';
+import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from 'simple-lightbox';
 
-// const galleryEl = document.querySelector('.gallery');
+const galleryEl = document.querySelector('.gallery');
 
-// const galleryItem = galleryItems
-//   .map(({ preview, original, description }) => {
-//     return `<a
-//         href="${original}"
-//         title="${description}"
-//         ><img
-//           src="${preview}"
-//           alt="${description}"
-//       /></a>`;
-//   })
-//   .join('');
-// galleryEl.insertAdjacentHTML('beforeend', galleryItem);
+const galleryItem = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a
+        href="${original}"
+        title="${description}"
+        ><img
+          src="${preview}"
+          alt="${description}"
+      /></a>`;
+  })
+  .join('');
+galleryEl.insertAdjacentHTML('beforeend', galleryItem);
 
-// new SimpleLightbox({ elements: '.gallery a' });
+new SimpleLightbox({ elements: '.gallery a' });
