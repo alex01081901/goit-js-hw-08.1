ac3e857c2f9e93c04fddf1481b5fcbb1d7c5c1bb
alex01081901 <114284497+alex01081901@users.noreply.github.com>
2023-04-04 15:58:06 +0300

    fh

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,15 @@
 // Add imports above this line
 // import '../../node_modules/simple-lightbox/dist/simpleLightbox.css';
 import "simplelightbox/dist/simple-lightbox.min.css"
+                
 import { galleryItems } from './gallery-items';
 // Change code below this line
 import   SimpleLightbox from "simplelightbox";
-
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+  scrollZoom: false,
+});
 const galleryEl = document.querySelector('.gallery');
 
 const galleryItem = galleryItems
